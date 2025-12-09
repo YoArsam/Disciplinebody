bd7d0948b753f484371d9a9311c7de029c61a1d4
tannaz homayounfar <tannazhomayounfar@Arsams-MacBook-Air.local>
2025-12-09 11:26:01 +0400

    Fix: use 100vh to fill from top to nav bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,7 +32,7 @@ function Home({
       
       setWidgetStyle({
         transform: `translateY(-${rect.top - safeTop}px)`,
-        height: `${rect.height + (rect.top - safeTop) + extraHeight + 1000}px`,
+        height: `calc(100vh - ${safeTop}px - ${navHeight}px)`,
       })
     } else {
       setWidgetStyle({})
