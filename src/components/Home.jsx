e3ed1406d849e00224b6117b0cb3aceb1589673c
tannaz homayounfar <tannazhomayounfar@Arsams-MacBook-Air-4.local>
2026-01-18 21:26:59 +0400

    UI: Reduce extra scroll space at bottom of habits list to h-24

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -501,7 +501,7 @@ function Home({
             })
           )}
           {/* Extra scrolling space at the bottom */}
-          <div className="h-64 w-full flex-shrink-0" />
+          <div className="h-24 w-full flex-shrink-0" />
         </div>
       </div>
     </div>
